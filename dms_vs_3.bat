42e61fb31f8be6546c4e0611299a4fe5e4f59814
naivete13sky <chen320821@163.com>
2022-12-17 18:17:34 +0800

    20221217 2

--- dms_vs_3.bat
+++ dms_vs_3.bat
@@ -1,7 +1,19 @@
-echo param0=%0
-echo param1=%1
+::echo param0=%0
+
+set my_param=%1
+::echo %my_param%
+
+set my_param_1=%my_param:~9%
+::echo %my_param_1%
+
+set my_param_2=%my_param_1:~0,-2%
+echo %my_param_2%
+
+pause
+python C:/cc/python/epwork/dms_vs_2/dms_vs_3.py  --int_job_id=%my_param_2%
+
+
+
 
 pause
 
-ping 127.0.0.1
-ping 127.0.0.1
